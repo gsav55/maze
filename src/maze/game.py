6fdf13bfc8d8e0bff0b58e0fcc86e4ab8f089786
gsav55 <graysav@gmail.com>
2024-12-01 13:24:58 -0500

    Added a maze and changed controls

--- src/maze/game.py
+++ src/maze/game.py
@@ -1,4 +1,5 @@
 import pygame
+from pathlib import Path
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -6,25 +7,23 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-# 1280/100 = 12.8
-# 729/100 = 7.2
-# 12 x 7 grid of 100x100 boxes to make map
-#
-grid = [
-    [1,1,1,1,1,1,1,1,1,1,1,1],
-    [0,0,1,0,0,0,1,0,0,0,0,1],
-    [1,0,1,0,1,0,0,0,1,0,0,1],
-    [1,0,1,0,1,1,1,0,1,0,0,1],
-    [1,0,0,0,1,0,1,0,1,0,0,1],
-    [1,0,1,0,0,0,1,0,0,0,0,0],
-    [1,1,1,1,1,1,1,1,1,1,1,1],
-]
+WALL = 0x000000
+FLOOR = 0xFFFFFF
+GOAL = 0xFF55FF
+START = 0x55FFFF
+
+
+assets = (Path(__file__).parent.parent / "assets").resolve()
+mazebg = pygame.image.load((assets / "maze-01").resolve()).convert()
+
+
+# 1280/40 = 32
+# 720/40 = 18
+# 32 x 18 grid of 40x40 boxes to make map
 
 # player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player_pos = pygame.Vector2(2,1)
+player_pos = pygame.Vector2(20, 20)
 
-for row in grid:
-    for 
 
 while running:
     # poll for events
@@ -33,8 +32,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYUP:
+            match event.key:
+                case pygame.K_w:
+                    player_pos.y -= 40
+                case pygame.K_UP:
+                    player_pos.y -= 40
+                case pygame.K_s:
+                    player_pos.y += 40
+                case pygame.K_DOWN:
+                    player_pos.y += 40
+                case pygame.K_a:
+                    player_pos.x -= 40
+                case pygame.K_LEFT:
+                    player_pos.x -= 40
+                case pygame.K_d:
+                    player_pos.x += 40
+                case pygame.K_RIGHT:
+                    player_pos.x += 40
+
     # Fill screen with a color to clear last frame
-    screen.fill("purple")
+    screen.blit(mazebg, (0, 0))
 
     # Render game here!
     pygame.draw.circle(screen, "red", player_pos, 40)
