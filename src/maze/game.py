7123dfe2a2d0a3a43040dd29d7ce125ebe0eb8da
gsav55 <graysav@gmail.com>
2024-12-01 14:09:53 -0500

    Tweaked maze and movement

--- src/maze/game.py
+++ src/maze/game.py
@@ -12,9 +12,10 @@ FLOOR = 0xFFFFFF
 GOAL = 0xFF55FF
 START = 0x55FFFF
 
+MOVE_PIXELS = 40
 
 assets = (Path(__file__).parent.parent / "assets").resolve()
-mazebg = pygame.image.load((assets / "maze-01").resolve()).convert()
+mazebg = pygame.image.load((assets / "maze-01.bmp").resolve()).convert()
 
 
 # 1280/40 = 32
@@ -22,8 +23,10 @@ mazebg = pygame.image.load((assets / "maze-01").resolve()).convert()
 # 32 x 18 grid of 40x40 boxes to make map
 
 # player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player_pos = pygame.Vector2(20, 20)
+player_pos = pygame.Vector2(80, 80)
 
+
+pygame.key.set_repeat(500, 100)
 
 while running:
     # poll for events
@@ -32,31 +35,32 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYDOWN:
             match event.key:
                 case pygame.K_w:
-                    player_pos.y -= 40
+                    player_pos.y -= MOVE_PIXELS
                 case pygame.K_UP:
-                    player_pos.y -= 40
+                    player_pos.y -= MOVE_PIXELS
                 case pygame.K_s:
-                    player_pos.y += 40
+                    player_pos.y += MOVE_PIXELS
                 case pygame.K_DOWN:
-                    player_pos.y += 40
+                    player_pos.y += MOVE_PIXELS
                 case pygame.K_a:
-                    player_pos.x -= 40
+                    player_pos.x -= MOVE_PIXELS
                 case pygame.K_LEFT:
-                    player_pos.x -= 40
+                    player_pos.x -= MOVE_PIXELS
                 case pygame.K_d:
-                    player_pos.x += 40
+                    player_pos.x += MOVE_PIXELS
                 case pygame.K_RIGHT:
-                    player_pos.x += 40
+                    player_pos.x += MOVE_PIXELS
 
     # Fill screen with a color to clear last frame
     screen.blit(mazebg, (0, 0))
 
     # Render game here!
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "red", player_pos, 30)
 
+    """
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
@@ -69,6 +73,7 @@ while running:
 
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+    """
 
     # Flip() display to put your work on screen
     pygame.display.flip()
